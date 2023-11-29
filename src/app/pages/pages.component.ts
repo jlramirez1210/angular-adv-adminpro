3abe68c9e812fd8efc36ede957b2c9476cad2513
jorge <jlramirez1210@gmail.com>
2023-11-29 09:33:04 -0500

    fin de seccion 7

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SettingsService} from "../services/settings.service";
 
+// @ts-ignore
+declare function customInitFunctions();
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styles: [
   ]
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit{
+  constructor(private settingsService: SettingsService) {
+  }
 
+  ngOnInit() {
+    customInitFunctions();
+  }
 }
